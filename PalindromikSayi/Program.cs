fcd987402f3941ed6f668eda083d93e8c6cf9fe2
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-17 21:43:38 +0300

    Add project files.

---
+++ PalindromikSayi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PalindromikSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TersiniAl = "";
+            List<int> PalindromikSayilar = new List<int>();
+            string[] sayilar = new string[10000];
+            for (int i = 1; i <=10000 ; i++)
+            {
+                for (int j = 0; j <i.ToString().Length ; j++)
+                {
+                  TersiniAl=  i.ToString().Substring(j, 1)+TersiniAl;
+                    sayilar[i - 1] = TersiniAl;
+
+                    
+                }
+                
+                TersiniAl = "";
+
+                if (i.ToString()==sayilar[i-1])
+                {
+                    Console.WriteLine(sayilar[i-1]);
+                }
+            }    
+        }
+    }
+}
